2c0d1fcf756057571eb1b54323e8cf95049d5bf9
Alex Hortua <alexander@datzsolutions.com>
2025-07-29 00:12:09 -0700

    Additionla jekyl config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./src/components/**/*.{js,jsx,ts,tsx}",
     "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: 'class',
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter', 'sans-serif'],
+        sans: ['Inter var', 'system-ui', 'sans-serif'],
       },
     },
   },
